3ac6b596ba60be022909358a5b6ce47566c766f0
Tanya <906030538@qq.com>
2025-02-12 04:00:27 +0800

    update model

--- src/model.ts
+++ src/model.ts
@@ -16,14 +16,37 @@ export interface Video {
     senddate: number | Date,
     title: string,
     description: string,
+    tag: Array<string>,
+    pic: string,
+    duration: number,
 }
+
+export interface VideoStat {
+    id: number,         // 稿件avid
+    cap_time: Date,     // 抓取时间
+    view: number,       // 播放数
+    danmaku: number,    // 弹幕数
+    reply: number,      // 评论数
+    favorite: number,   // 收藏数
+    coin: number,       // 投币数
+    share: number,      // 分享数
+    now_rank: number,   // 当前排名
+    his_rank: number,   // 历史最高排行
+    like: number,       // 获赞数
+    dislike: number,    // 0 点踩数
+    evaluation: string, // 视频评分
+    vt: number,         // 0
+}
+
 export interface Subscribe {
     id: number,
 }
+
 export interface Rule {
     id: number,
     sid: number,
 }
+
 export interface Source {
     id: number,
     sid: number,
@@ -41,5 +64,7 @@ export function db(ctx: Context) {
         senddate: 'timestamp',
         title: 'string',
         description: 'text',
+        tag: 'array',
+        pic: 'string',
     })
 }
